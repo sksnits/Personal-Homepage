45d390d81278170544b6bcc5d97faa530a92a28a
iamgithub1999 <66543406+iamgithub1999@users.noreply.github.com>
2020-08-12 00:59:25 +0530

    Update Footer.js
    Simply removed the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,15 @@
+/*
 import React from 'react'
 import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
-      /*  <p className="copyright">
+       <p className="copyright">
          Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="https://surath-kumar-srivastva.netlify.app/"> Surath Srivastva</a>  
         <br/>
         Get your own version of this webpage <a href="https___github.com/adityaketkar/circle-packing-personal-homepage"> here </a>
-        </p>*/
+        </p>
     </footer>
 )
 
@@ -16,4 +17,4 @@ Footer.propTypes = {
     timeout: PropTypes.bool
 }
 
-export default Footer
+export default Footer*/
