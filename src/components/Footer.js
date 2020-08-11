1ccd4bf4be907484a118c2f4099a3af308921b1e
iamgithub1999 <66543406+iamgithub1999@users.noreply.github.com>
2020-08-12 00:36:12 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types'
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
-        <p className="copyright">
+      /*  <p className="copyright">
          Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="https://surath-kumar-srivastva.netlify.app/"> Surath Srivastva</a>  
         //<br/>
-       /* Get your own version of this webpage <a href="https___github.com/adityaketkar/circle-packing-personal-homepage"> here </a>*/
-        </p>
+       /* Get your own version of this webpage <a href="https___github.com/adityaketkar/circle-packing-personal-homepage"> here </a>
+        </p>*/
     </footer>
 )
 
